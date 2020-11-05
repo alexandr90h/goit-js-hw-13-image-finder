5dfd8d1733e09df6b6dfde1fa06a4b1797160c02
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2020-11-05 17:28:04 +0200

    05.11.2020-2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import './styles.scss';
+import * as basicLightbox from 'basiclightbox';
 import galeryListTempl from './templates/galery.hbs';
 import ImageApiService from './js/query-service.js';
 
+// const basicLightbox = require('basiclightbox');
 const refs = {
     galleryElem: document.querySelector('.gallery'),
     searchFormElem: document.querySelector('.search-form'),
     btnLoadMore:document.querySelector('.btn-load-more')
 };
 const imageApiService = new ImageApiService();
-
+// const instanse = basicLightbox();
 refs.searchFormElem.addEventListener('submit', onSearch);
 refs.btnLoadMore.addEventListener('click', onLoadMore);
+refs.galleryElem.addEventListener('click', onOpenModal);
 function onSearch(e) {
     e.preventDefault();
     imageApiService.query = e.currentTarget.elements.query.value;
@@ -21,14 +24,21 @@ function onSearch(e) {
 }
 function onLoadMore() {
     imageApiService.fetchArticles().then(appArtMarkup);
-    window.scrollTo({
-    top: innerHeight,
-    behavior: "smooth"
-});
+//     window.scrollTo({
+//     top: innerHeight,
+//     behavior: "smooth"
+// });
 }
 function appArtMarkup (hits) {
     refs.galleryElem.insertAdjacentHTML('beforeend', galeryListTempl(hits));
 }
 function clearArtMarkup () {
     refs.galleryElem.innerHTML = '';
+}
+function onOpenModal(e) {
+    const elem = e.target;
+    console.log(elem);
+    const instance = basicLightbox.create(elem);
+    instance.show();
+    console.log(instance);
 }
