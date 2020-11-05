2e557ecb2ca07ffcd93054b48e569373e051d997
Olexandr <alexandr90h@gmail.com>
2020-11-05 23:50:45 +0300

    05.11.2020-3

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import * as basicLightbox from 'basiclightbox';
 import galeryListTempl from './templates/galery.hbs';
 import ImageApiService from './js/query-service.js';
 
-// const basicLightbox = require('basiclightbox');
 const refs = {
     galleryElem: document.querySelector('.gallery'),
     searchFormElem: document.querySelector('.search-form'),
     btnLoadMore:document.querySelector('.btn-load-more')
 };
 const imageApiService = new ImageApiService();
-// const instanse = basicLightbox();
 refs.searchFormElem.addEventListener('submit', onSearch);
 refs.btnLoadMore.addEventListener('click', onLoadMore);
 refs.galleryElem.addEventListener('click', onOpenModal);
@@ -18,9 +16,8 @@ function onSearch(e) {
     e.preventDefault();
     imageApiService.query = e.currentTarget.elements.query.value;
     imageApiService.resetPage();
-    imageApiService.fetchArticles().then(appArtMarkup);
+    imageApiService.fetchArticles().then(appArtMarkup).finally(refs.btnLoadMore.style.display = 'block');
     clearArtMarkup();
-    refs.btnLoadMore.style.display = 'block';
 }
 function onLoadMore() {
     imageApiService.fetchArticles().then(appArtMarkup);
@@ -36,9 +33,10 @@ function clearArtMarkup () {
     refs.galleryElem.innerHTML = '';
 }
 function onOpenModal(e) {
+    if (e.target.nodeName !== "IMG") {
+    return;
+  }
     const elem = e.target;
-    console.log(elem);
-    const instance = basicLightbox.create(elem);
-    instance.show();
-    console.log(instance);
+    const instance = basicLightbox.create(`<img src=${elem.getAttribute('data-url')}/>`);
+ instance.show();
 }
